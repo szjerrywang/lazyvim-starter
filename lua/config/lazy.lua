234f6f815828bb387136e534ce06884fe2406f45
Jerry Wang <szjerrywang@163.com>
2024-04-08 13:34:58 +0800

    modify treesitter configuration.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -3,7 +3,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://gh-proxy.com/https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -28,7 +28,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,8 @@
+return {{
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = {"bash", "html", -- "javascript",
+        "json", "lua", "markdown", "markdown_inline", "python", "query", "regex", -- "tsx", "typescript",
+        "vim", "yaml"}
+    }
+}}
