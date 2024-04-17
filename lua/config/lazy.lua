b5f882166e8f79952c8bb0a8269ff2eb6e849525
Jerry Wang <szjerrywang@163.com>
2024-04-17 21:49:58 +0800

    Update lazy.lua
    add gh-proxy.com

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -3,7 +3,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://gh-proxy.com/https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
