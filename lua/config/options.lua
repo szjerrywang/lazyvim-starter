995f5dce2030f21dff0891d6d20bf3a557d556fe
Jerry Wang <szjerrywang@163.com>
2024-04-18 16:42:59 +0800

    config lsp inlay

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- if vim.lsp.inlay_hint then
+--  vim.lsp.inlay_hint.enable(0, true)
+-- end
