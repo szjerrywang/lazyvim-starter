80339263fb5c38ea5f202acbfc79bd4a27e3a541
Jerry Wang <szjerrywang@163.com>
2024-05-08 22:08:34 +0800

    add lsp codelens extensions plugin

--- lua/plugins/lsp-codelens.lua
+++ lua/plugins/lsp-codelens.lua
@@ -1,5 +1,7 @@
+
 return {
     'E-ricus/lsp_codelens_extensions.nvim',
+    -- lazy = false,
     dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
     config = function ()
       require("codelens_extensions").setup()
